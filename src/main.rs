6a1e7c054ca80c130efccf694008c201df85e692
Simon Clark <simonclark236@gmail.com>
2020-08-04 18:52:16 -0400

    building out general algo

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,41 @@ use std::io::BufRead;
 use core::fmt;
 use std::collections::HashSet;
 
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    static ref ALL_OPTIONS: HashSet<u32> = vec![1, 2, 3, 4, 5, 6, 7, 8, 9].into_iter().collect();
+}
+
 fn main() {
     // sudokus_from_files("sudokus/0_Easy_000.adk");
     let line = "600079020940103000000400000000002054205806100070000000000000000000600538400000063 #Easy".to_string();
     let s = sudoku_from_line(line);
     println!("{}", s);
-    println!("{:?}", square(&s.board, 3, 4));
-    println!("{:?}", find_first_empty(&s.board))
+    println!("{:?}", square_taken(&s.board, 3, 4));
+    println!("{:?}", find_first_empty(&s.board));
+    solve_sudoku(s);
 }
 
-fn solve_sudoku(input: Sudoku) {
-
+fn solve_sudoku(input: Sudoku) -> Option<Sudoku> {
+    return match find_first_empty(&input.board) {
+        None => Some(input),
+        Some((row, col)) => {
+            let options = ALL_OPTIONS
+                .difference(&col_taken(&input.board, col))
+                .map(|item| *item)
+                .collect::<HashSet<u32>>()
+                .difference(&row_taken(&input.board, row))
+                .map(|item| *item)
+                .collect::<HashSet<u32>>()
+                .difference(&square_taken(&input.board, row, col))
+                .map(|item| *item)
+                .collect::<Vec<u32>>();
+            println!("{:?}", options);
+            return None;
+        }
+    }
 }
 
 fn sudoku_from_line(line: String) -> Sudoku {
@@ -27,7 +51,7 @@ fn sudoku_from_line(line: String) -> Sudoku {
         .collect()};
 }
 
-fn column(board: &[u32], column: u32) -> HashSet<u32> {
+fn col_taken(board: &[u32], column: u32) -> HashSet<u32> {
     return board
         .into_iter()
         .map(|item| *item)
@@ -37,17 +61,17 @@ fn column(board: &[u32], column: u32) -> HashSet<u32> {
         .collect();
 }
 
-fn row(board: &[u32], row: u32) -> HashSet<u32> {
+fn row_taken(board: &[u32], row: u32) -> HashSet<u32> {
     return board
         .into_iter()
         .map(|item| *item)
         .skip((row * 9) as usize)
         .take(9)
         .filter(|n| *n != 0)
-        .collect::<HashSet<_>>();
+        .collect();
 }
 
-fn square(board: &[u32], row: u32, col: u32) -> HashSet<u32> {
+fn square_taken(board: &[u32], row: u32, col: u32) -> HashSet<u32> {
     let start = (row / 3 * 27 + col / 3 * 3) as usize;
     return [&board[start..start+3],
         &board[start+9..start+12],
