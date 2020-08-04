234a618aff8b8efc3d6fa43ca3d65487d8f978c8
Simon Clark <simonclark236@gmail.com>
2020-08-03 22:05:38 -0400

    board printing

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use core::fmt;
 fn main() {
     // sudokus_from_files("sudokus/0_Easy_000.adk");
     let line = "600079020940103000000400000000002054205806100070000000000000000000600538400000063 #Easy".to_string();
-    let v = sudoku_from_line(line);
-    println!("{:?}", v.board.len());
+    let s = sudoku_from_line(line);
+    println!("{}", s);
 }
 
 fn sudoku_from_line(line: String) -> Sudoku {
@@ -20,14 +20,46 @@ fn sudoku_from_line(line: String) -> Sudoku {
         .collect()};
 }
 
+fn column(input: Sudoku, column: u32) -> Vec<u32> {
+    return input.board
+        .into_iter()
+        .skip(column as usize)
+        .step_by(9)
+        .collect();
+}
 
-
+fn row(input: Sudoku, row: u32) -> Vec<u32> {
+    return input.board
+        .into_iter()
+        .skip((row * 9) as usize)
+        .take(9)
+        .collect();
+}
 
 struct Sudoku{board: Vec<u32>}
 
 impl fmt::Display for Sudoku{
     fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        write!(f, "{}", self.board[0])
+        write!(f, "{}", &self.board
+            .chunks(9)
+            .map(|row| row
+                .to_vec()
+                .chunks(3)
+                .map(|block| block
+                    .to_vec()
+                    .into_iter()
+                    .map(|item| item.to_string())
+                    .collect::<Vec<String>>()
+                    .join(" "))
+                .collect::<Vec<String>>()
+                .join(" | "))
+            .collect::<Vec<String>>()
+            .chunks(3)
+            .map(|group_of_rows| group_of_rows
+                .to_vec()
+                .join("\n"))
+            .collect::<Vec<String>>()
+            .join("\n---------------------\n"))
     }
 }
 
